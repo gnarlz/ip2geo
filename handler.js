d7dba8d6587ca132e0858051974313acf9d37e37
Thomas Yates <tom@ip2geo.co>
2020-04-07 06:40:46 -0500

    pulled setResponseHeadersCORS and enrichRequest out into module

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ const rateLimiter = require('./lib/rateLimiter');
 const requestCounter = require('./lib/requestCounter');
 const ip2geo  = require('./lib/ip2geo');
 const ip2asn  = require('./lib/ip2asn');
-const moment = require('moment');
+const utilities  = require('./utility/utilities');
 
 module.exports.lookup = (event, context) => {
     return new Promise((resolve, reject) => {
@@ -19,9 +19,9 @@ module.exports.lookup = (event, context) => {
             ip = event['requestContext']['identity']['sourceIp'];
         }
         //loadTest(ip, key);   // uncomment for quick and dirty load testing
-
-        enrichRequest(request, event, context, ip);
-        setResponseHeadersCORS(response);   // enable CORS in api gateway when using lambda proxy integration
+        request.lookup_ip = ip;
+        utilities.enrichRequest(request, event, context);
+        utilities.setResponseHeadersCORS(response);   // enable CORS in api gateway when using lambda proxy integration
 
         Promise.all([validate.ip(ip), validate.key(key) ])
             .then(([ipResult, keyResult]) =>{
@@ -45,34 +45,6 @@ module.exports.lookup = (event, context) => {
             });
     });
 };
-
-
-
-function enrichRequest(request, event, context, ip){
-    request.lookup_ip = ip;
-    request.request_id = context.awsRequestId;
-    request.request_ts = moment().format('YYYY-MM-DD HH:mm:ss.SSSSSS');
-    request.source_ip = event['requestContext']['identity']['sourceIp'];
-    request.is_desktop = (event['headers']['CloudFront-Is-Desktop-Viewer'] === "true");
-    request.is_mobile = (event['headers']['CloudFront-Is-Mobile-Viewer'] === "true");
-    request.is_smart_tv = (event['headers']['CloudFront-Is-SmartTV-Viewer'] === "true");
-    request.is_tablet = (event['headers']['CloudFront-Is-Tablet-Viewer'] === "true");
-    request.viewer_country = event['headers']['CloudFront-Viewer-Country'];
-    request.accept_language = event['headers']['Accept-Language'];
-    request.origin = event['headers']['origin'];
-    request.referer = event['headers']['Referer'];
-    request.user_agent = event['headers']['User-Agent'];
-}
-
-function setResponseHeadersCORS(response){
-    response.headers = {
-        "X-Requested-With": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
-    };
-}
-
 
 function createErrorResponse(request, response, payload, err, start){
     payload.time_elapsed = new Date() - start;

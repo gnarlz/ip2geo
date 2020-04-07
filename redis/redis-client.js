6321d033f76b0729b770132ecb6263ea082da1fd
Thomas Yates <tom@ip2geo.co>
2020-04-06 19:30:39 -0500

    standardized redis client creation

--- redis/redis-client.js
+++ redis/redis-client.js
@@ -1,31 +1,19 @@
 'use strict';
 
 const redis = require('redis');
-const redis_mock = require("redis-mock");
 
 let client;
-
-if(process.env.REDIS_PORT){
-    client = redis.createClient({
+client = redis.createClient({
         port      : process.env.REDIS_PORT,
         host      : process.env.REDIS_IP_ADDRESS,
         password  : process.env.REDIS_PASS
-    });
-    client.on('connect', function() {
-        console.log("redis client - connected");
-    });
-    client.on('error', function (err) {
-        console.error("redis client - error:" + err);
-    });
-} else {
-    client = redis_mock.createClient();
-    client.on('connect', function() {
-        console.log("mock redis client - connected");
-    });
-    client.on('error', function (err) {
-        console.error("mock redis client - error:" + err);
-    });
-}
+});
+client.on('connect', function() {
+    console.log("redis client - connected");
+});
+client.on('error', function (err) {
+    console.error("redis client - error:" + err);
+});
 
 // localhost
 // client    = redis.createClient({});

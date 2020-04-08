acdf36208696f3ec9781d434da735303acfee5c0
Thomas Yates <tom@ip2geo.co>
2020-04-08 05:54:45 -0500

    renamed validate.eventBody to validate.subscriptionEventBody

--- subscribe/subscribe.js
+++ subscribe/subscribe.js
@@ -35,7 +35,7 @@ function subscribe(event, planID){
         subscription_data.planID = planID;
         utilities.setResponseHeadersCORS(response);   // enable CORS in api gateway when using lambda proxy integration
 
-        validate.eventBody(event.body)
+        validate.subscriptionEventBody(event.body)
             .then(() => {
                 const params = new URLSearchParams(event.body);
                 subscription_data.plan_name = params.get("plan_name");

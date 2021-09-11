62e35e0291dfedbb1bf675347b3133db5918c6c2
Thomas Yates <thomasyates@northwesternmutual.com>
2021-09-11 07:42:47 -0500

    lib/requestCounter: refactor + unit tests

---
+++ test/unit/lib/requestCounter-spec.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const expect  = require("chai").expect
+const proxyquire = require('proxyquire')
+
+const unit = (fns) => {
+    return proxyquire('../../../lib/requestCounter', {
+        '../postgres/postgres-client': {
+            query: fns.query || (it => it)
+        }   
+  })
+}
+
+describe('lib/requestHelper test',() => {
+    it('should return null when query() is successful in increment()', () => {
+        const requestCounterProxy = unit({ query: async (data) => {return null} })
+        return requestCounterProxy.increment('abc123', 'requestId-12345')
+        .then( (response) => {expect(response).to.be.null} )
+    })
+    it('should return null when query() is unsuccessful in increment()', () => {
+        const requestCounterProxy = unit({ query: async (data) => {throw new Error()} })
+        return requestCounterProxy.increment('def456', 'requestId-12345')
+        .then( (response) => {expect(response).to.be.null} )
+    })
+})
